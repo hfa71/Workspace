18641f2d97009cfa1e3d837edd88205e67cf256a
hfa71 <79068618+hfa71@users.noreply.github.com>
2021-06-19 18:10:32 -0400

    Update HFA-OSDCould.ps1

--- OSDCloud/HFA-OSDCould.ps1
+++ OSDCloud/HFA-OSDCould.ps1
@@ -1,1 +1,26 @@
+Write-Host  -ForegroundColor Yellow "Starting el OSD Personalizado de Hermann ..."
+cls
+Write-Host "================ Menu Principal =====================" -ForegroundColor Yellow
+Write-Host "===============  Aerocenter Chile ===================" -ForegroundColor Yellow
+Write-Host "=================== @hermannf =======================" -ForegroundColor Yellow
+Write-Host "=====================================================" -ForegroundColor Yellow
+Write-Host "1: Zero-Touch Win10 21H1 | English | Professional"-ForegroundColor Yellow
+Write-Host "2: Zero-Touch Win10 21H1 | Spanish | Professional" -ForegroundColor Yellow
+Write-Host "3: Yo lo seleccionalre Manual"-ForegroundColor Yellow
+Write-Host "4: Salir`n"-ForegroundColor Yellow
+$input = Read-Host "Seleccionar"
 
+Write-Host  -ForegroundColor Yellow "Cargando OSDCloud..."
+
+Import-Module OSD -Force
+Install-Module OSD -Force
+
+switch ($input)
+{
+    '1' { Start-OSDCloud -OSLanguage en-us -OSBuild 21H1 -OSEdition Enterprise -ZTI } 
+    '2' { Start-OSDCloud -OSLanguage en-us -OSBuild 20H2 -OSEdition Enterprise -ZTI } 
+    '3' { Start-OSDCloud	} 
+    '4' { Exit		}
+}
+
+wpeutil reboot
