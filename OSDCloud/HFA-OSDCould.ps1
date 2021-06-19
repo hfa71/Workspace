7c09d3fbe64af3ec5b60c50228b36f4ed586d2c0
hfa71 <79068618+hfa71@users.noreply.github.com>
2021-06-19 18:30:53 -0400

    Update HFA-OSDCould.ps1

--- OSDCloud/HFA-OSDCould.ps1
+++ OSDCloud/HFA-OSDCould.ps1
@@ -4,7 +4,7 @@ Write-Host "================ Menu Principal =====================" -ForegroundCo
 Write-Host "===============  Aerocenter Chile ===================" -ForegroundColor Yellow
 Write-Host "=================== @hermannf =======================" -ForegroundColor Yellow
 Write-Host "=====================================================" -ForegroundColor Yellow
-Write-Host "1: Zero-Touch Win10 21H1 | English | Professional"-ForegroundColor Yellow
+Write-Host "1: Zero-Touch Win10 21H1 | English | Professional" -ForegroundColor Yellow
 Write-Host "2: Zero-Touch Win10 21H1 | Spanish | Professional" -ForegroundColor Yellow
 Write-Host "3: Yo lo seleccionalre Manual"-ForegroundColor Yellow
 Write-Host "4: Salir`n"-ForegroundColor Yellow
@@ -17,8 +17,8 @@ Install-Module OSD -Force
 
 switch ($input)
 {
-    '1' { Start-OSDCloud -OSLanguage en-us -OSBuild 21H1 -OSEdition Professional -ZTI } 
-    '2' { Start-OSDCloud -OSLanguage es-mx -OSBuild 21H1 -OSEdition Professional -ZTI } 
+    '1' { Start-OSDCloud -OSLanguage en-us -OSBuild 21H1 -OSEdition Professional -ZTI -EnableNetFX } 
+    '2' { Start-OSDCloud -OSLanguage es-mx -OSBuild 21H1 -OSEdition Professional -ZTI -EnableNetFX } 
     '3' { Start-OSDCloud	} 
     '4' { Exit		}
 }
